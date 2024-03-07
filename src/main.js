5c3c4011797ef8bd09758fd3a6b1cbfc148a3420
Frtavonatti <frtavonatti@gmail.com>
2024-03-07 07:58:14 -0300

    Maqueteado y conexión a API: Sección de trending Movies

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+const API_URL = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
+
+document.addEventListener("DOMContentLoaded", getTrendingMoviesPreview)
+
+async function getTrendingMoviesPreview () {
+    const res = await fetch (API_URL)
+    const data = await res.json()
+
+    const movies = data.results
+    movies.forEach(element => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        const container = document.createElement("div")
+        container.classList.add("movie-container")
+
+        const image = document.createElement("img")
+        image.classList.add("movie-img")
+        image.setAttribute(
+            'src',
+            'https://image.tmdb.org/t/p/w300' + element.poster_path,
+          );
+
+        container.append(image)
+        trendingPreviewMoviesContainer.append(container)
+    });
+}
---
+++ src/secret.js
@@ -0,0 +1,1 @@
+const API_KEY = "dd135397ab1c56097c69096d0db9f687"
